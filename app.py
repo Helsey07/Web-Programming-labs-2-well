f1775b5846da8d7c175b9498eabdd366f8fa4cb8
Петровичев Егор <petrushka.love07@mail.ru>
2023-09-13 16:53:52 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -5,5 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 
 def start():
-    return "web-сервер на flask"
-    
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                Петровичев Егор Васильевич, Лабораторная 1
+            </title>
+        </head>
+
+        <body>
+            <header>
+                НГТУ, ФБ, Лабораторная работа 1
+            </header>
+
+            <h1>
+                web-сервер на flask
+            </h1>
+
+            <footer>
+                &copy; Петровичев Егор, ФБИ-13, 3 курс, 2023
+            </footer>
+        </body>
+    </html>
+    """
