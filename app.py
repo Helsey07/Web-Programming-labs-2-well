81a290ed77b7aac8bf1a4dadf5598237c9152fab
Петровичев Егор <petrushka.love07@mail.ru>
2023-09-27 17:33:03 +0700

    Книги

--- app.py
+++ app.py
@@ -200,5 +200,17 @@ def example():
         {'name':'манго', 'price': 100}
     ]
 
+    books = [
+         {'name':'Название 1', 'autor': 'Автор 1', 'genre': 'жанр 1', 'pages': 228},
+         {'name':'Название 2', 'autor': 'Автор 2', 'genre': 'жанр 2', 'pages': 2238},
+         {'name':'Название 3', 'autor': 'Автор 3', 'genre': 'жанр 3', 'pages': 228},
+         {'name':'Название 4', 'autor': 'Автор 4', 'genre': 'жанр 4', 'pages': 2128},
+         {'name':'Название 5', 'autor': 'Автор 5', 'genre': 'жанр 5', 'pages': 2428},
+         {'name':'Название 6', 'autor': 'Автор 6', 'genre': 'жанр 6', 'pages': 228},
+         {'name':'Название 7', 'autor': 'Автор 7', 'genre': 'жанр 7', 'pages': 2528},
+         {'name':'Название 8', 'autor': 'Автор 8', 'genre': 'жанр 8', 'pages': 2628},
+         {'name':'Название 9', 'autor': 'Автор 9', 'genre': 'жанр 9', 'pages': 2128},
+         {'name':'Название 10', 'autor': 'Автор 10', 'genre': 'жанр 10', 'pages': 2218}
+    ]
     return render_template('example.html', name=name, number_lab=number_lab, group=group, course=course,
-                           fruits=fruits)
+                           fruits=fruits, books=books)
