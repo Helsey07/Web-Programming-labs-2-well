406d7f628feb164f93fce976503b0824afa9bfa9
Петровичев Егор <petrushka.love07@mail.ru>
2023-11-21 16:33:34 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -20,9 +20,32 @@ def login():
 
     if username == 'alex' and password == '123':
        return render_template('success_login.html', username=username)
-    if not username:
+    elif not username:
         return render_template('login.html', error_2=error_2, username=username, password=password)
     elif not password:
         return render_template('login.html', error_3=error_3, username=username, password=password)
     elif request.method == 'POST':
         return render_template('login.html', error=error, username=username, password=password)
+    
+@lab4.route('/lab4/fridge', methods = ['GET', 'POST'])
+def fridge():
+    temperature = request.form.get('temperature')
+    message = ""
+
+    if request.method == 'POST':
+        if not temperature:
+            message = "Ошибка: не задана температура"
+        else:
+            temperature = int(temperature)
+            if temperature < -12:
+                message = "Не удалось установить температуру — слишком низкое значение"
+            elif temperature > -1:
+                message = "Не удалось установить температуру — слишком высокое значение"
+            elif -12 <= temperature <= -9:
+                message = f"Установлена температура: {temperature}°С"
+            elif -8 <= temperature <= -5:
+                message = f"Установлена температура: {temperature}°С"
+            elif -4 <= temperature <= -1:
+                message = f"Установлена температура: {temperature}°С"
+
+    return render_template('fridge.html', message=message, temperature=temperature)
