c97578d3cc270b52fbd2152fa3e3384326884393
Петровичев Егор <petrushka.love07@mail.ru>
2024-01-07 19:56:46 +0700

    поздравления

--- lab9.py
+++ lab9.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, abort, render_template, request, jsonify
-
+from flask import Blueprint, render_template, request, abort, jsonify, redirect, url_for
 lab9 = Blueprint('lab9', __name__)
 
-@lab9.route('/lab9/')
+
+@lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+
+    if request.method == 'POST':
+        sender_name = request.form.get('sender_name')
+        recipient_gender = request.form.get('recipient_gender')
+        recipient_name = request.form.get('recipient_name')
+
+        return redirect(url_for('lab9.card', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name))
+    else:
+        return render_template('lab9/index.html')
+
+
+@lab9.route('/lab9/card', methods=['GET'])
+def card():
+    sender_name = request.args.get('sender_name')
+    recipient_gender = request.args.get('recipient_gender')
+    recipient_name = request.args.get('recipient_name')
+
+    return render_template('lab9/card.html', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name)
 
 
 @lab9.app_errorhandler(404)
